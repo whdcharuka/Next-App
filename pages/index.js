2b3a30f8015fa32e2d72eb803b5e7a47e87864dd
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-14 11:03:07 +0530

    modified

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
-import Head from 'next/head'
+import { server } from '../config'
+import ArticleList from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
-      <Head>
-        <title>About</title>
-      </Head>
-      <h1>Welcome to next</h1>
+      <ArticleList articles={articles} />
     </div>
   )
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
+}
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   }
+// }
