932071b14d96c2a7583a5d556e8e1cde325321a4
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-14 11:01:55 +0530

    modified

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,16 @@
-import headerStyles from '../styles/Header.module.css';
+import headerStyles from '../styles/Header.module.css'
 
 const Header = () => {
-    const x = 2
-    return (
-        <div>
-            <h1 className={headerStyles.tittle}>
-                <span>Web Dev</span>News
-            </h1>
-            <p className={headerStyles.description}>
-                Keep up date with the latest web dev news
-            </p>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className={headerStyles.title}>
+        <span>WebDev</span> News
+      </h1>
+      <p className={headerStyles.description}>
+        Keep up to date with the latest web dev news
+      </p>
+    </div>
+  )
 }
 
 export default Header
